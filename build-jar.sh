6513a7f4759d89107efe8f07803809d3c3f85098
ehsansaleh <ehsan.slh@gmail.com>
2017-05-20 15:49:56 -0500

    Delete build-jar.sh

--- build-jar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-thisDir=$(dirname $0) || false
-
-pushd ${thisDir} > /dev/null
-    gradle clean build shadowJar
-    cp build/libs/StatAlign-3.2-all.jar ./StatAlign.jar
-popd > /dev/null
